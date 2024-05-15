aecfc966494cd7056c7014c222ed4b14bcba77e4
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2024-05-14 22:24:21 -0400

    updated README & small change in test

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 
-
 # TODO
 
 - [x] change UI
@@ -7,3 +6,44 @@
 - [ ] fix prepare-docs
 - [ ] dockerize
 - [ ] accesibility
+
+
+# Getting Started
+
+## Start Ollama
+
+```sh
+# create the model
+ollama create <name> -f '.\Modelfile - Llama2'
+
+
+# the machine that runs ollama have to set this env variables
+OLLAMA_HOST  =  0.0.0.0
+OLLAMA_ORIGINS = *
+OLLAMA_KEEP_ALIVE = 30
+
+# run ollama
+ollama serve
+```
+
+## Start Postgres (pg-vector)
+
+```sh
+# download pv-vector (w/ vpn)
+docker pull ankane/pgvector:latest
+
+# move to project folder
+docker compose up -d
+```
+
+## Start webpage
+
+```sh
+pnpm dev
+
+# or
+pnpm dev --turbo
+
+# or
+npx next dev --turbo
+```
--- src/lib/langchain/__tests__/llm.test.ts
+++ src/lib/langchain/__tests__/llm.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "vitest";
 import { loadEvaluator } from "langchain/evaluation";
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
 
 export const model = new ChatOllama({
 	baseUrl: "http://127.0.0.1:11434",
@@ -8,19 +9,58 @@ export const model = new ChatOllama({
 	temperature: 0,
 });
 
+export const embedding = new OllamaEmbeddings({
+	baseUrl: "http://127.0.0.1:11434",
+	model: "mistral",
+});
+
 const evaluator = await loadEvaluator("criteria", {
 	criteria: "conciseness",
 	llm: model,
+	embedding,
 });
 
-test("suma 2 + 2 es igual a 3", async () => {
-	const res = await evaluator.evaluateStrings({
-		input: "Cuanto es 2+2?",
-		prediction:
-			"Cuanto es 2+2? Esta es una pregunta basica. La respuesta que estas buscando es que dos mas dos es cuatro",
-	});
+// test("suma 2 + 2 es igual a 3", async () => {
+// 	const res = await evaluator.evaluateStrings({
+// 		input: "Cuanto es 2+2?",
+// 		prediction:
+// 			"Cuanto es 2+2? Esta es una pregunta basica. La respuesta que estas buscando es que dos mas dos es cuatro",
+// 	});
 
-	console.log({ res });
+// 	expect(res.score).toBe(1);
+// });
 
-	expect(res.score).toBe(1);
+// test("", async () => {
+// 	const res = await evaluator.evaluateStrings({
+// 		input: "¿Qué se espera lograr a través del proceso base de Gestión de Procesos de la Organización (GPO)?",
+// 		prediction: `
+//         a) Definir los procesos de la organización que permitan conducir a la organización al cumplimiento de su misión y objetivos estratégicos.
+//         b) Establecer una infraestructura tecnológica.
+//         c) Elaborar una propuesta estándar de los procesos de la organización.
+//         `,
+// 	});
+
+// 	expect(res.score).toBe(1);
+// });
+
+test.each([
+	// {
+	// 	name: "suma 2 + 2 es igual a 3",
+	// 	input: "Cuanto es 2+2?",
+	// 	prediction: `Cuanto es 2+2? Esta es una pregunta basica.
+	//         La respuesta que estas buscando es que dos mas dos es cuatro`,
+	// },
+	{
+		name: "",
+		input: "¿Qué se espera lograr a través del proceso base de Gestión de Procesos de la Organización (GPO)?",
+		prediction: `
+         a) Definir los procesos de la organización que permitan conducir a la organización al cumplimiento de su misión y objetivos estratégicos.
+         b) Establecer una infraestructura tecnológica.
+         c) Elaborar una propuesta estándar de los procesos de la organización.
+         `,
+	},
+])("$name", async ({ input, prediction }) => {
+	const res = await evaluator.evaluateStrings({ input, prediction });
+	if (res.score !== 1) console.log(res.reasoning);
+	expect(res.score).toBe(0);
 });
